2e91f24621b7486b083018bb8837cdbb2ba2810a
CodexMika <mokwamikaela@gmail.com>
2024-08-10 20:28:32 +0000

    adicionar pagina inicial 1

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function home() {
+    return <h1>Oi, meu anjo! Você está lendo essa mensagem que eu programei 
+        especialmente para você. Se gostou e ta com saudades da uma risadinha</h1>
+}
+
+export default home;
